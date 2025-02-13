b3cdd90146878d33fecc7f1c55b6e2b87310ec73
Diana <diana.synentschenko@gmail.com>
2025-02-13 09:53:45 +0100

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Goal from "./components/Goal.tsx";
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goal.jpg";
 
 export default function App()
 {
   return (
   <main>
+    <Header image = {{ src: goalsImg, alt: 'A list of goals'}}>
+      <h1>Your goals</h1>
+    </Header>
     <Goal title="Some Title" descriptin="Some Description"/>
-    <Goal title="Another Title" descriptin="Another Description"/>
+    
   </main>
   );
 }
